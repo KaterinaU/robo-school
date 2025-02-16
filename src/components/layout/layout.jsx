8e3dc1ae92ca483aa409b3bd5af8db0507062d25
KaterinaU <ushakova.esu@gmail.com>
2025-02-17 00:03:50 +0500

    feat: add layout

---
+++ src/components/layout/layout.jsx
@@ -0,0 +1,12 @@
+import { Footer } from '@/components/footer/index';
+import { Header } from '@/components/header/index';
+
+import styles from './layout.module.scss';
+
+export const Layout = ({ children }) => (
+    <div className={styles.layout}>
+      <Header />
+      <main className={styles.main}>{children}</main>
+      <Footer />
+    </div>
+  );
