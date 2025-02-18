3a2b20d540e0afe11c8d30ae75e0c7b4eae11c55
KaterinaU <ushakova.esu@gmail.com>
2025-02-18 21:35:38 +0500

    fix: fix prettier

--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -4,9 +4,9 @@ import { Header } from '@/components/header/index';
 import styles from './layout.module.scss';
 
 export const Layout = ({ children }) => (
-    <div className={styles.layout}>
-      <Header />
-      <main className={styles.main}>{children}</main>
-      <Footer />
-    </div>
-  );
+  <div className={styles.layout}>
+    <Header />
+    <main className={styles.main}>{children}</main>
+    <Footer />
+  </div>
+);
