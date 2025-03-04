ff18ae6cf8c6a185de598a21a06f3be347ac11d3
KaterinaU <ushakova.esu@gmail.com>
2025-03-04 23:56:08 +0500

    fix: add useState, TooltipIcon, text to Tooltip and remove subtitleContainer from BenefitItem

--- src/components/tooltip/tooltip.jsx
+++ src/components/tooltip/tooltip.jsx
@@ -1,10 +1,30 @@
+import { useState } from 'react';
+
+import { TooltipIcon } from '@/assets/icons/TooltipIcon';
+
 import styles from './tooltip.module.scss';
 
-export const Tooltip = ({ text, icon }) => {
+export const Tooltip = ({ text }) => {
+  const [isShowTooltip, setIsShowTooltip] = useState(false);
+
+  const onMouseEnterHandler = () => {
+    setIsShowTooltip(true);
+  };
+
+  const onMouseLeaveHandler = () => {
+    setIsShowTooltip(false);
+  };
+
   return (
-    <div className={styles.tooltipContainer}>
-      <div className={styles.tooltipTrigger}>{icon}</div>
-      <div className={styles.tooltipContent}>{text}</div>
-    </div>
+    <span
+      className={styles.tooltipContainer}
+      onMouseEnter={onMouseEnterHandler}
+      onMouseLeave={onMouseLeaveHandler}
+    >
+      <span className={styles.tooltipTrigger}>
+        <TooltipIcon />
+      </span>
+      {isShowTooltip && <span className={styles.tooltipContent}>{text}</span>}
+    </span>
   );
 };
--- src/pages/main-page/components/main-benefits/components/benefit-item/benefit-item.jsx
+++ src/pages/main-page/components/main-benefits/components/benefit-item/benefit-item.jsx
@@ -1,15 +1,14 @@
-import { TooltipIcon } from '@/assets/icons';
-import { Tooltip } from '@/components/tooltip/';
+import { Tooltip } from '@/components/tooltip';
 
 import styles from './benefit-item.module.scss';
 
 export const BenefitItem = ({ benefitItemData: { id, subtitle, description, tooltip } }) => {
   return (
     <div key={id} className={styles.benefitItem}>
-      <div className={styles.subtitleContainer}>
-        <h3 className={styles.subtitle}>{subtitle}</h3>
-        {tooltip?.length && <Tooltip text={tooltip} icon={<TooltipIcon />} />}
-      </div>
+      <h3 className={styles.subtitle}>
+        {subtitle}
+        {tooltip?.length && <Tooltip text={tooltip} />}
+      </h3>
       <p className={styles.description}>{description}</p>
     </div>
   );
