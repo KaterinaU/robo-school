f43b3c02c8d72ef19d346436a82ee691feef6923
KaterinaU <ushakova.esu@gmail.com>
2025-03-17 00:19:05 +0500

    feat: add input

--- src/components/input/input.jsx
+++ src/components/input/input.jsx
@@ -1,34 +1,20 @@
-import styles from './Input.module.scss'; // Подключаем SCSS модуль
+import styles from './Input.module.scss';
 
-export const Input = ({
-                        id,
-                        type = 'text',
-                        placeholder,
-                        register,
-                        error,
-                        label,
-                        ...rest
-                      }) => {
+export const Input = ({ id, type = 'text', placeholder, register, error, label, ...rest }) => {
   return (
     <div className={styles.inputWrapper}>
-      {label && (
-        <label htmlFor={id} className={styles.label}>
-          {label}
-        </label>
-      )}
+      {label && <label className={styles.label}>{label}</label>}
 
       <input
         id={id}
         type={type}
         placeholder={placeholder}
         className={`${styles.input} ${error ? styles.error : ''}`}
-        {...(register && register(id))} // Интеграция с react-hook-form
+        {...(register && register(id))}
         {...rest}
       />
 
-      {error && (
-        <span className={styles.errorMessage}>{error.message}</span>
-      )}
+      {error && <span className={styles.errorMessage}>{error.message}</span>}
     </div>
   );
 };
