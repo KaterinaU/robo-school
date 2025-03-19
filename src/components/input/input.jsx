6b9fb8140faeb3b0ab43b2dbeb03c57bacb4c1ee
KaterinaU <ushakova.esu@gmail.com>
2025-03-20 00:55:47 +0500

    fix: add inputClassName to Input

--- src/components/input/input.jsx
+++ src/components/input/input.jsx
@@ -1,20 +1,17 @@
 import styles from './input.module.scss';
 
-export const Input = ({ id, type = 'text', placeholder, register, error, label, ...rest }) => {
+export const Input = ({ id, type = 'text', placeholder, error, label, ...rest }) => {
+  const getInputClassName = (baseClass, errorClass, error) => {
+    return error ? `${baseClass} ${errorClass}` : baseClass;
+  };
+  const inputClassName = getInputClassName(styles.input, styles.error, error);
   return (
     <div className={styles.inputWrapper}>
-      {label && <label className={styles.label}>{label}</label>}
+      {Boolean(label) && <label className={styles.label}>{label}</label>}
 
-      <input
-        id={id}
-        type={type}
-        placeholder={placeholder}
-        className={`${styles.input} ${error ? styles.error : ''}`}
-        {...(register && register(id))}
-        {...rest}
-      />
+      <input id={id} type={type} placeholder={placeholder} className={inputClassName} {...rest} />
 
-      {error && <span className={styles.errorMessage}>{error.message}</span>}
+      {Boolean(error) && <span className={styles.errorMessage}>{error.message}</span>}
     </div>
   );
 };
