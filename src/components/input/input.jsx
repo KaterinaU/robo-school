e427cea2eeadb79c6b7a51ab72247034c09c9f09
KaterinaU <ushakova.esu@gmail.com>
2025-03-20 16:58:32 +0500

    fix: fix input

--- src/components/input/input.jsx
+++ src/components/input/input.jsx
@@ -1,17 +1,29 @@
+import { forwardRef } from 'react';
+
 import styles from './input.module.scss';
 
-export const Input = ({ id, type = 'text', placeholder, error, label, ...rest }) => {
-  const getInputClassName = (baseClass, errorClass, error) => {
-    return error ? `${baseClass} ${errorClass}` : baseClass;
-  };
-  const inputClassName = getInputClassName(styles.input, styles.error, error);
-  return (
-    <div className={styles.inputWrapper}>
-      {Boolean(label) && <label className={styles.label}>{label}</label>}
+export const Input = forwardRef(
+  ({ id, type = 'text', placeholder, error, label, ...rest }, ref) => {
+    const getInputClassName = (baseClass, errorClass, error) => {
+      return error ? `${baseClass} ${errorClass}` : baseClass;
+    };
+    const inputClassName = getInputClassName(styles.input, styles.error, error);
+    return (
+      <div className={styles.inputWrapper}>
+        {Boolean(label) && <label className={styles.label}>{label}</label>}
 
-      <input id={id} type={type} placeholder={placeholder} className={inputClassName} {...rest} />
+        <input
+          id={id}
+          type={type}
+          placeholder={placeholder}
+          className={inputClassName}
+          ref={ref}
+          {...rest}
+        />
 
-      {Boolean(error) && <span className={styles.errorMessage}>{error.message}</span>}
-    </div>
-  );
-};
+        {Boolean(error) && <span className={styles.errorMessage}>{error.message}</span>}
+      </div>
+    );
+  },
+);
+Input.displayName = 'Input';
