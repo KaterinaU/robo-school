f6d6a3e40fafc163369518cacff6ddb12de75780
KaterinaU <ushakova.esu@gmail.com>
2025-03-26 22:11:09 +0500

    fix: add emailRegex to schema

--- src/pages/main-page/components/main-form/helpers/schema.js
+++ src/pages/main-page/components/main-form/helpers/schema.js
@@ -2,6 +2,7 @@ import * as yup from 'yup';
 
 const nameRegex = /^[А-Яа-яЁё\s-]+$/;
 const phoneRegex = /^\+7\d{10}$/;
+const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
 
 export const schema = yup.object().shape({
   name: yup
@@ -13,5 +14,9 @@ export const schema = yup.object().shape({
     .string()
     .required('Телефон обязателен')
     .matches(phoneRegex, 'Неверный формат телефона'),
-  email: yup.string().required('Email обязателен').email('Неверный формат email'),
+  email: yup
+    .string()
+    .required('Email обязателен')
+    .email('Неверный формат email')
+    .matches(emailRegex, 'Неверный формат email'),
 });
