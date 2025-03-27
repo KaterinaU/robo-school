256d2c4d2272718025d6642d447d11db5325af25
KaterinaU <ushakova.esu@gmail.com>
2025-03-27 22:20:08 +0500

    fix: fix input

--- src/components/input/input.jsx
+++ src/components/input/input.jsx
@@ -1,28 +1,29 @@
 import styles from './input.module.scss';
 
-export const Input = ({ id, type = 'text', placeholder, error, label, inputRef, ...rest }) => {
-  const getInputClassName = (baseClass, errorClass, error) => {
+export const Input = ({ id, type = 'text', placeholder, label, register, error, ...rest }) => {
+  const getInputClassName = () => {
     if (error) {
-      return `${baseClass} ${errorClass}`;
+      return `${styles.input} ${styles.error}`;
     }
-    return baseClass;
+    return styles.input;
   };
-
-  const inputClassName = getInputClassName(styles.input, styles.error, error);
 
   return (
     <div className={styles.inputWrapper}>
-      {Boolean(label) && <label className={styles.label}>{label}</label>}
+      {Boolean(label) && (
+        <label htmlFor={id} className={styles.label}>
+          {label}
+        </label>
+      )}
 
       <input
         id={id}
         type={type}
         placeholder={placeholder}
-        className={inputClassName}
-        ref={inputRef}
+        className={getInputClassName()}
+        {...register}
         {...rest}
       />
-
       {Boolean(error) && <span className={styles.errorMessage}>{error.message}</span>}
     </div>
   );
