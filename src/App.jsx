98209b76c0ea19fd17a39ac6880b09d4bce5dd19
KaterinaU <ushakova.esu@gmail.com>
2025-04-15 23:59:00 +0500

    fix: fix scrollbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
+
+import 'swiper/scss';
+import 'swiper/scss/scrollbar';
+import 'swiper/scss/mousewheel';
+
 import { Layout } from '@/components/layout';
 import { MainPage } from '@/pages/main-page';
 
+import './styles/swiper-overrides.scss';
 import './styles/index.scss';
 
 export const App = () => (
