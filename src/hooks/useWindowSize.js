3794ff833550c72ffcc60d31d52b778de914017d
KaterinaU <ushakova.esu@gmail.com>
2025-04-15 23:55:44 +0500

    feature: add isMobile to useWindowSize

--- src/hooks/useWindowSize.js
+++ src/hooks/useWindowSize.js
@@ -1,22 +1,16 @@
 import { useEffect, useState } from 'react';
 
 export const useWindowSize = () => {
-  const [windowSize, setWindowSize] = useState({
-    width: window.innerWidth,
-    height: window.innerHeight,
-  });
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
 
   useEffect(() => {
     const handleResize = () => {
-      setWindowSize({
-        width: window.innerWidth,
-        height: window.innerHeight,
-      });
+      setIsMobile(window.innerWidth <= 768);
     };
 
     window.addEventListener('resize', handleResize);
     return () => window.removeEventListener('resize', handleResize);
   }, []);
 
-  return windowSize;
+  return { isMobile };
 };
