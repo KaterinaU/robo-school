0a743f06f4ed46fd4a41940ba9de89c7d5a56e4a
KaterinaU <ushakova.esu@gmail.com>
2025-04-23 00:25:01 +0500

    fix: remove swiper-overrides.scss'

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import 'swiper/scss/mousewheel';
 import { Layout } from '@/components/layout';
 import { MainPage } from '@/pages/main-page';
 
-import './styles/swiper-overrides.scss';
 import './styles/index.scss';
 
 export const App = () => (
