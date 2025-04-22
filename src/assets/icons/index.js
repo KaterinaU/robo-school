b073fdcae7cb42dde3ca77ada364d1b21e2f0348
KaterinaU <ushakova.esu@gmail.com>
2025-04-23 00:26:06 +0500

    fix: fix export Icons

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -2,5 +2,7 @@ import { BurgerIcon } from './BurgerIcon';
 import { CloseIcon } from './CloseIcon';
 import { PhoneIcon } from './PhoneIcon';
 import { TooltipIcon } from './TooltipIcon';
+export { ArrowLeftIcon } from './ArrowLeftIcon';
+export { ArrowRightIcon } from './ArrowRightIcon';
 
 export { BurgerIcon, CloseIcon, PhoneIcon, TooltipIcon };
